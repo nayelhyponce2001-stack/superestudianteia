80b38d43479a69fdec9ff40ef8822b94f10c9f97
Nayelhy Ponce <nayelhy@gmail.com>
2026-05-15 19:09:23 -0500

    nuevo diseño home

--- src/counter.js
+++ src/counter.js
@@ -1,30 +1,21 @@
 export function setupCounter(element) {
+
   let counter = 0
+
   const setCounter = (count) => {
+
     counter = count
-    element.innerHTML = `Count is ${counter}`
+
+    element.innerHTML = `contador ${counter}`
+
   }
-  element.addEventListener('click', () => setCounter(counter + 1))
+
+  element.addEventListener('click', () => {
+
+    setCounter(counter + 1)
+
+  })
+
   setCounter(0)
 
-  document.querySelector('#btnFracciones')
-.addEventListener('click', ()=>{
-
-  content.innerHTML = fraccionesPage()
-
-})
-
-document.querySelector('#btnBalance')
-.addEventListener('click', ()=>{
-
-  content.innerHTML = balancePage()
-
-})
-
-document.querySelector('#btnEnergia')
-.addEventListener('click', ()=>{
-
-  content.innerHTML = energiaPage()
-
-})
 }
